7d97491037581fcea332c9b926c25c85426f7f74
Evgeniy Putilin <mr.putilinz@gmail.com>
2023-10-05 16:27:33 +0300

    added changed  files

--- src/main/java/ru/putilin/hibernate/controller/MyController.java
+++ src/main/java/ru/putilin/hibernate/controller/MyController.java
@@ -5,6 +5,7 @@ import ru.putilin.hibernate.entity.Persons;
 import ru.putilin.hibernate.repository.MyRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/persons/by-city")
 @RestController
@@ -20,6 +21,18 @@ public class MyController {
     @GetMapping()
     public List<Persons> getPersonByCity(@RequestParam("city") String city) {
         return myRepository.getPersonByCity(city);
-
     }
+
+    @GetMapping("/age")
+    public List<Persons> getPersonsByAge(@RequestParam("age") int age ) {
+        return myRepository.getPersonsByAge(age);
+    }
+
+    @GetMapping("/name")
+    public Persons getPersonsByNameAndSurname(@RequestParam("name") String name,
+                                                        @RequestParam("surname") String surname) {
+        return myRepository.getPersonsByNameAndSurname(name, surname).
+                orElseThrow();
+    }
+
 }
--- src/main/java/ru/putilin/hibernate/repository/MyRepository.java
+++ src/main/java/ru/putilin/hibernate/repository/MyRepository.java
@@ -1,28 +1,38 @@
 package ru.putilin.hibernate.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.putilin.hibernate.entity.Persons;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MyRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
 
-    public MyRepository() {
+
+    private final PersonsRepository personsRepository;
+
+
+    public MyRepository(PersonsRepository personsRepository) {
+        this.personsRepository = personsRepository;
     }
 
     public List<Persons> getPersonByCity(String city) {
 
-        String query = "SELECT persons FROM Persons persons where lower(persons.city) ilike ?1";
-
-        List<Persons> list = entityManager.createQuery(query, Persons.class)
-                .setParameter(1, city)
-                .getResultList();
-        return list;
+        return personsRepository.findByCityIgnoreCase(city);
     }
+
+    public List<Persons> getPersonsByAge(int age) {
+       return personsRepository.findByPerson_AgeLessThan(age, Sort.by("person.age"));
+
+    }
+
+    public Optional<Persons> getPersonsByNameAndSurname(String name, String surname) {
+        return personsRepository.findByPerson_NameAndPerson_Surname(name, surname);
+
+    }
+
+
 
 }
